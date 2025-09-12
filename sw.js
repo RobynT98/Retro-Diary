55866f395f8a45d4b3d0b9105e08894f9f45a438
Conri Turesson <50335159+RobynT98@users.noreply.github.com>
2025-09-12 20:00:29 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,50 @@
-const CACHE_NAME = "retro-diary-mem1";
-const ASSETS = [
-  "./","./index.html",
-  "./styles.css?v=mem1","./app.js?v=mem1","./fonts_db.js?v=mem1",
-  "./leather.jpg","./parchment.jpg",
-  "./manifest.json","./icon-192.png","./icon-512.png"
+const CACHE_NAME = "retro-diary-v2";
+const CORE_ASSETS = [
+  "./",
+  "index.html",
+  "styles.css",
+  "app.js",
+  "fonts_db.js",
+  "manifest.json",
+  "sw.js",
+  "icon-192.png",
+  "icon-512.png",
+  "leather.jpg",
+  "parchment.jpg"
 ];
 
-self.addEventListener("install", e=>{
-  e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)));
-});
-self.addEventListener("activate", e=>{
-  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));
-});
-self.addEventListener("fetch", e=>{
-  e.respondWith(
-    fetch(e.request).then(res=>{
-      const copy=res.clone();
-      caches.open(CACHE_NAME).then(c=>c.put(e.request, copy));
-      return res;
-    }).catch(()=>caches.match(e.request))
+// Install – lägg i cache
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(CORE_ASSETS))
   );
+  self.skipWaiting();
+});
+
+// Activate – rensa gamla cachar
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((k) => (k !== CACHE_NAME ? caches.delete(k) : null)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch – försök nät först, annars cache
+self.addEventListener("fetch", (event) => {
+  const url = new URL(event.request.url);
+
+  // Endast egna filer cachas. Externa (YouTube, bilder via URL, ljud mm) släpps igenom.
+  if (url.origin === location.origin) {
+    event.respondWith(
+      fetch(event.request)
+        .then((resp) => {
+          const copy = resp.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+          return resp;
+        })
+        .catch(() => caches.match(event.request))
+    );
+  }
 });
