ef8c012f2a30f4edb0cd2d49d45d6f7b2c8ff968
Conri Turesson <50335159+RobynT98@users.noreply.github.com>
2025-09-12 14:00:46 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,32 +1,35 @@
-const CACHE_NAME = "retro-diary-book16";
+const CACHE_NAME = "retro-diary-v1";
 const ASSETS = [
-  "./",
-  "./index.html",
-  "./styles.css?v=book12",
-  "./app.js?v=book12",
-  "./leather.jpg",
-  "./parchment.jpg",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png"
+  "index.html",
+  "styles.css?v=1",
+  "app.js?v=1",
+  "manifest.json?v=1",
+  "icon-192.png",
+  "icon-512.png"
 ];
 
-self.addEventListener("install", e=>{
-  e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)));
-});
-
-self.addEventListener("activate", e=>{
+self.addEventListener("install", e => {
+  self.skipWaiting();
   e.waitUntil(
-    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))))
+    caches.open(CACHE_NAME).then(c => c.addAll(ASSETS))
   );
 });
 
-self.addEventListener("fetch", e=>{
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))
+    ).then(()=>self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", e => {
+  if(e.request.method!=="GET") return;
   e.respondWith(
-    fetch(e.request).then(res=>{
-      const copy=res.clone();
+    fetch(e.request).then(r=>{
+      const copy = r.clone();
       caches.open(CACHE_NAME).then(c=>c.put(e.request, copy));
-      return res;
+      return r;
     }).catch(()=>caches.match(e.request))
   );
 });
