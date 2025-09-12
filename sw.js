3e515e7a5d93af41483d65bd67f9d37f461a6ac5
Conri Turesson <50335159+RobynT98@users.noreply.github.com>
2025-09-12 16:31:44 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,10 @@
-const CACHE_NAME = "retro-diary-book13";
+const CACHE_NAME = "retro-diary-bookX";
 const ASSETS = [
   "./",
   "./index.html",
-  "./styles.css?v=book13",
-  "./app.js?v=book13",
+  "./styles.css?v=bookX",
+  "./app.js?v=bookX",
+  "./fonts.js?v=bookX",
   "./leather.jpg",
   "./parchment.jpg",
   "./manifest.json",
@@ -13,20 +14,20 @@ const ASSETS = [
 
 self.addEventListener("install", e=>{
   e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)));
+  self.skipWaiting();
 });
 
 self.addEventListener("activate", e=>{
   e.waitUntil(
-    caches.keys().then(keys=>Promise.all(
-      keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))
-    ))
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))))
   );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", e=>{
   e.respondWith(
     fetch(e.request).then(res=>{
-      const copy = res.clone();
+      const copy=res.clone();
       caches.open(CACHE_NAME).then(c=>c.put(e.request, copy));
       return res;
     }).catch(()=>caches.match(e.request))
