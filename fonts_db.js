17f15ed4fa0dedc3d3d4ed5a0badb85ed51fe5ec
Conri Turesson <50335159+RobynT98@users.noreply.github.com>
2025-09-12 17:01:55 +0200

    Update and rename fonts.js to fonts_db.js

---
+++ fonts_db.js
@@ -0,0 +1,19 @@
+// En enkel font-"databas". label = vad som visas i listan, value = CSS font-family
+window.FONT_DB = [
+  { label: 'Special Elite', value: "'Special Elite', cursive" },
+  { label: 'IM Fell English', value: "'IM Fell English', serif" },
+  { label: 'Dancing Script', value: "'Dancing Script', cursive" },
+  { label: 'Roboto Slab', value: "'Roboto Slab', serif" },
+  { label: 'Merriweather', value: "'Merriweather', serif" },
+  { label: 'Playfair Display', value: "'Playfair Display', serif" },
+  { label: 'Lora', value: "'Lora', serif" },
+  { label: 'Crimson Text', value: "'Crimson Text', serif" },
+  { label: 'Cardo', value: "'Cardo', serif" },
+  { label: 'Spectral', value: "'Spectral', serif" },
+  { label: 'Kalam', value: "'Kalam', cursive" },
+  { label: 'Courgette', value: "'Courgette', cursive" },
+  { label: 'Handlee', value: "'Handlee', cursive" },
+  { label: 'Cormorant Garamond', value: "'Cormorant Garamond', serif" },
+  { label: 'Source Serif 4', value: "'Source Serif 4', serif" },
+  { label: 'DM Serif Display', value: "'DM Serif Display', serif" }
+];
