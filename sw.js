a8086b6be063fb92909067f19500eb02abfcade9
Conri Turesson <50335159+RobynT98@users.noreply.github.com>
2025-09-12 04:42:37 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Retro Diary Service Worker (lite/book)
-const CACHE_NAME = "retro-diary-v3";
+const CACHE_NAME = "retro-diary-v4";
 const ASSETS = [
   "/",             // index.html
   "/index.html",
