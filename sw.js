01bfac7268a7fa5cf7ed10b64a4ac175ad1486e5
Conri Turesson <50335159+RobynT98@users.noreply.github.com>
2025-09-12 21:07:48 +0200

    Refactor service worker for caching strategy

--- sw.js
+++ sw.js
@@ -1,29 +1,56 @@
-const CACHE_NAME = "retro-diary-rich21";
-const ASSETS = [
-  "./",
-  "./index.html",
-  "./styles.css?v=rich21",
-  "./app.js?v=rich21",
-  "./fonts_db.json?v=rich21",
-  "./leather.jpg",
-  "./parchment.jpg",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png"
+// sw.js – Retro Diary
+const CACHE_NAME = "retro-diary-v25"; // ändra versionsnummer vid varje uppdatering
+const CORE_ASSETS = [
+  "/",               // index.html
+  "/index.html",
+  "/styles.css",
+  "/app.js",
+  "/fonts_db.js",
+  "/manifest.json",
+  "/sw.js",
+  "/leather.jpg",
+  "/parchment.jpg",
+  "/icon-192.png",
+  "/icon-512.png"
 ];
 
-self.addEventListener("install", e=>{
-  e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)));
+// Installera: cacha grundfiler
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(CORE_ASSETS))
+  );
+  self.skipWaiting();
 });
-self.addEventListener("activate", e=>{
-  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));
+
+// Aktivera: ta bort gamla cacher
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
 });
-self.addEventListener("fetch", e=>{
+
+// Hämta: nätverk först, fallback till cache
+self.addEventListener("fetch", (e) => {
+  const url = new URL(e.request.url);
+
+  // Hoppa över tredjepartsresurser (YouTube, Google Fonts, osv)
+  if (url.origin !== location.origin) {
+    return;
+  }
+
   e.respondWith(
-    fetch(e.request).then(res=>{
-      const copy=res.clone();
-      caches.open(CACHE_NAME).then(c=>c.put(e.request, copy));
-      return res;
-    }).catch(()=>caches.match(e.request))
+    fetch(e.request)
+      .then((res) => {
+        // Lägg i cache enbart GET-resurser
+        if (e.request.method === "GET") {
+          const clone = res.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(e.request, clone));
+        }
+        return res;
+      })
+      .catch(() => caches.match(e.request))
   );
 });
